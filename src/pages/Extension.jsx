40908c37a386e7c543174f8c5047274224675754
NatureSon22 <blendingworker@gmail.com>
2024-05-24 20:24:07 +0800

    setup extension section

--- src/pages/Extension.jsx
+++ src/pages/Extension.jsx
@@ -1,7 +1,26 @@
+import ExtensionCard from "../components/ExtensionCard";
+import { extensions } from "../constants";
+
 const Extension = () => {
   return (
-    <div>Extension</div>
-  )
-}
+    <section className="py-20 grid place-items-center gap-2">
+      <div className="w-[90%] text-center">
+        <p className="font-semibold text-[1.6rem] leading-10 text-dark-blue">
+          Download the extension
+        </p>
+        <p className="mt-3 mb-7 text-grayish-blue leading-7">
+          We’ve got more browsers in the pipeline. Please do let us know if
+          you’ve got a favourite you’d like us to prioritize.
+        </p>
+      </div>
 
-export default Extension
+      <div className="w-[75%] grid place-items-center grid-cols-1 gap-10">
+        {extensions.map((extension) => (
+          <ExtensionCard key={extension.id} {...extension}></ExtensionCard>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Extension;
