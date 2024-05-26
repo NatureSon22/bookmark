ce4d860f37229a08ad7474d4627d7e1ef25d3792
NatureSon22 <blendingworker@gmail.com>
2024-05-26 14:38:17 +0800

    added open-close functionality

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,15 +1,26 @@
 import { close, facebook, mobileBookMark, twitter } from "../constants/imports";
 
-const MobileNav = () => {
+const MobileNav = ({ openNav, setOpenNav }) => {
+  const handleClick = () => {
+    setOpenNav(false);
+  };
+
   return (
-    <div className="w-screen h-screen px-8 py-12 bg-dark-blue/95 flex flex-col fixed top-0 left-0 z-20">
+    <div
+      className={`w-screen h-screen px-8 py-10 bg-dark-blue/95 flex flex-col fixed top-0 left-0 z-20 transition-transform duration-500 transfrom  ${openNav ? "translate-x-0" : "translate-x-full"}`}
+    >
       <div className="mb-10 flex justify-between items-center">
         <img
           className="w-[10em] brightness-0 invert cursor-pointer"
           src={mobileBookMark}
           alt="Mobile Bookmark"
         />
-        <img className="cursor-pointer" src={close} alt="Close Icon" />
+        <img
+          className={`cursor-pointer transition-transform duration-500 transform ${openNav ? "rotate-180" : "rotate-0"}`}
+          src={close}
+          alt="Close Icon"
+          onClick={() => handleClick()}
+        />
       </div>
 
       <nav className="w-full self-center">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,18 @@
 import { iconHamburger, logoBookMark } from "../constants/imports";
 
-const NavBar = () => {
+const NavBar = ({ openNav, setOpenNav }) => {
+  const handleClick = () => {
+    setOpenNav(true);
+  };
+
   return (
     <nav className="flex justify-between items-center px-7 py-10">
       <img src={logoBookMark} alt="logo bookmark" />
       <img
-        className="cursor-pointer"
+        className={`cursor-pointer transition-transform duration-300 transform ${openNav ? "rotate-180" : "rotate-0"}`}
         src={iconHamburger}
         alt="hamburger icon"
+        onClick={() => handleClick()}
       />
     </nav>
   );
