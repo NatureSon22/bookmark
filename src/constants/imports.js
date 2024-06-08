dbf02197905926d7321b42d28d01d7947769bfe4
NatureSon22 <blendingworker@gmail.com>
2024-06-08 15:20:41 +0800

    added constants

--- src/constants/imports.js
+++ src/constants/imports.js
@@ -3,7 +3,37 @@ import logoChrome from "../assets/images/logo-chrome.svg";
 import iconHamburger from "../assets/images/icon-hamburger.svg";
 import hero from "../assets/images/illustration-hero.svg";
 import tab1 from "../assets/images/illustration-features-tab-1.svg";
-import tab2 from "../assets/images/illustration-features-tab-1.svg";
-import tab3 from "../assets/images/illustration-features-tab-1.svg";
+import tab2 from "../assets/images/illustration-features-tab-2.svg";
+import tab3 from "../assets/images/illustration-features-tab-3.svg";
+import logoGoogle from "../assets/images/logo-chrome.svg";
+import logoFirefox from "../assets/images/logo-firefox.svg";
+import logoOpera from "../assets/images/logo-opera.svg";
+import decorDot from "../assets/images/bg-dots.svg";
+import arrow from "../assets/images/icon-arrow.svg";
+import error from "../assets/images/icon-error.svg";
+import footer from "../assets/images/footer-logo.png";
+import twitter from "../assets/images/icon-twitter.svg";
+import facebook from "../assets/images/icon-facebook.svg";
+import mobileBookMark from "../assets/images/mobile-logo.png";
+import close from "../assets/images/icon-close.svg";
 
-export { logoBookMark, logoChrome, iconHamburger, hero, tab1, tab2, tab3 };
+export {
+  logoBookMark,
+  logoChrome,
+  iconHamburger,
+  hero,
+  tab1,
+  tab2,
+  tab3,
+  logoGoogle,
+  logoFirefox,
+  logoOpera,
+  decorDot,
+  arrow,
+  error,
+  footer,
+  twitter,
+  facebook,
+  mobileBookMark,
+  close,
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,19 +1,77 @@
-import { tab1, tab2, tab3 } from "./imports";
+import {
+  logoFirefox,
+  logoGoogle,
+  logoOpera,
+  tab1,
+  tab2,
+  tab3,
+} from "./imports";
 
 export const features = [
-    {
-        label: "Bookmark in one click",
-        info: "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
-        img: tab1
-    },
-    {
-        label: "Intelligent search",
-        info: "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
-        img: tab2
-    },
-    {
-        label: "Share your bookmarks",
-        info: "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
-        img: tab3
-    }
-]
+  {
+    id: crypto.randomUUID(),
+    label: "Bookmark in one click",
+    info: "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
+    img: tab1,
+  },
+  {
+    id: crypto.randomUUID(),
+    label: "Intelligent search",
+    info: "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+    img: tab2,
+  },
+  {
+    id: crypto.randomUUID(),
+    label: "Share your bookmarks",
+    info: "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+    img: tab3,
+  },
+];
+
+export const extensions = [
+  {
+    id: crypto.randomUUID(),
+    name: "Chrome",
+    img: logoGoogle,
+    minimumVersion: 62,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Firefox",
+    img: logoFirefox,
+    minimumVersion: 55,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Chrome",
+    img: logoOpera,
+    minimumVersion: 46,
+  },
+];
+
+export const faqs = [
+  {
+    id: crypto.randomUUID(),
+    question: "What is Bookmark?",
+    answer:
+      "Bookmark is a tool to organize and manage your favorite web pages. It helps you save, categorize, and quickly access important websites. With Bookmark, you can sync your bookmarks across devices, making your web experience seamless and efficient.",
+  },
+  {
+    id: crypto.randomUUID(),
+    question: "How can I request a new browser?",
+    answer:
+      "To request a new browser, go to our support page and fill out the request form. Provide details about the browser you need and any specific features you're looking for. Our team will review your request and get back to you with further information. We value user feedback and strive to support a wide range of browsers.",
+  },
+  {
+    id: crypto.randomUUID(),
+    question: "How do I reset my password?",
+    answer:
+      "If you need to reset your password, click on the 'Forgot Password' link on the login page. Enter your registered email address, and we will send you instructions to reset your password. Follow the steps in the email to create a new password and regain access to your account.",
+  },
+  {
+    id: crypto.randomUUID(),
+    question: "What about other Chromium browsers?",
+    answer:
+      "Other Chromium browsers, like Edge and Opera, are fully supported by Bookmark. You can install our extension and use all the features without any issues. We are committed to providing a consistent experience across all major browsers, ensuring you can manage your bookmarks easily, no matter which browser you prefer.",
+  },
+];
