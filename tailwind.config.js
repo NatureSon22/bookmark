03869b5c59e1cb4b3f8d82be6af9467fce8d62d5
NatureSon22 <blendingworker@gmail.com>
2024-06-08 15:21:24 +0800

    add classname formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ export default {
         "soft-blue": "hsl(231, 69%, 60%)",
         "soft-red": "hsl(0, 94%, 66%)",
         "grayish-blue": "hsl(229, 8%, 60%)",
-        "dark-blue": "hsl(229, 31%, 21%)"
+        "dark-blue": "hsl(229, 31%, 21%)",
       },
       fontFamily: {
-        "rubik": ["Rubik", "sans-serif"]
+        rubik: ["Rubik", "sans-serif"],
+      },
+      boxShadow: {
+        custom:
+          "rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+      },
+      screens: {
+        
       }
     },
   },
